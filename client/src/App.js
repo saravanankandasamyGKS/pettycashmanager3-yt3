9901314e43b9d723ff355a014d4a000ea2bc6a5a
saravanankandasamyGKS <gksaravanan008@gmail.com>
2024-06-16 15:49:03 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import axios from "axios"
+
 
 
 function App() {
